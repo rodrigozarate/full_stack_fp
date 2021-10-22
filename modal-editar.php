f89dabe33e866116d118a3446bd41815e55446c8
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-10-22 16:37:54 -0500

    [CREATE]

---
+++ modal-editar.php
@@ -0,0 +1,25 @@
+  <!-- Modal -->
+  <div class="modal fade" id="modalEditar" tabindex="-1" role="dialog" aria-labelledby="ModalEditar" aria-hidden="true">  
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="modalLabelEditar">Editar</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        
+        <div class="modal-body" id="cuerpov">
+        <?php // la inclusion del formulario se hace con JS ?>
+        </div>
+        
+        <div class="modal-footer">
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  
+  
+
+  
---
+++ modal.php
@@ -0,0 +1,18 @@
+  <!-- Modal -->
+  <div class="modal fade" id="modalNuevo" tabindex="-1" role="dialog" aria-labelledby="ModalNuevo" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="modalLabel">Titulo</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body" id="cuerpox">
+        <?php // include('frm_nuevo_usuario.php'); ?>
+        </div>
+        <div class="modal-footer">
+        </div>
+      </div>
+    </div>
+  </div>
